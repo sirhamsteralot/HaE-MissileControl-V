b0ff7327eb605a3e5074d04a519f3fd5830c3943
sirhamsteralot <sirhamsteralot@gmail.com>
2025-08-06 20:26:52 +0200

    system.numerics >:(

--- Missile.cs
+++ Missile.cs
@@ -9,7 +9,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using VRage;
 using VRage.Collections;
